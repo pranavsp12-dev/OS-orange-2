2c39c7941d00f2fd6b9e75d268944070f3e350a5
PRANAV SP <PES1UG24CS337@stu.pes.edu>
2026-04-19 21:14:58 +0530

    Handled parent commit linking logic

--- commit.c
+++ commit.c
@@ -191,7 +191,7 @@ int head_update(const ObjectID *new_commit) {
 //   - commit_serialize  : converts the filled Commit struct to a text buffer
 //   - object_write      : saves the serialized text as OBJ_COMMIT
 //   - head_update       : moves the branch pointer to your new commit
-//
+//C
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
     ObjectID tree_id;
